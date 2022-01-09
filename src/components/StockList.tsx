200579079e93af214e56fab2596a7441a8110a5c
Giltroy P. Meren <giltroy.meren@GFI00245.local>
2022-01-09 16:13:30 +0800

    Implemented basic connect via websocket

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -1,19 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import StockContext from '../context/StockContext'
 
 const StockList = () => {
-  const { stockList } = useContext(StockContext)
+  const { stockList, connectToServer } = useContext(StockContext)
+
+  useEffect(() => {
+    connectToServer()
+  })
 
   if (stockList.length === 0) return <h3>Nothing to show...</h3>
 
   return (
-    <table>
-      <tbody>
-        {stockList.map((item: string, index: number) => {
-          return <tr key={index}><td>{item}</td></tr>
-        })}
-      </tbody>
-    </table>
+    <>
+      <table>
+        <tbody>
+          {stockList.map((item: string, index: number) => {
+            return <tr key={index}><td>{item}</td></tr>
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
 
--- src/context/StockContext.tsx
+++ src/context/StockContext.tsx
@@ -1,16 +1,53 @@
+import React, { createContext, useReducer, useRef } from "react"
 import StockReducer, { ACTION_ADD_STOCK } from "./StockReducer";
 
-const { createContext, useReducer } = require("react");
+const DEFAULT_URL = `ws://159.89.15.214:8080/`
 
-const StockContext = createContext()
+export interface IStockContext {
+  stockList: string[],
+  connectToServer: any,
+  addStock: any,
+}
+
+const initialState = {
+  stockList: [],
+  connectToServer: () => {},
+  addStock: () => { },
+}
+
+const StockContext = createContext<IStockContext>(initialState)
 
 export const StockProvider: React.FC = ({ children }) => {
-  const initialState = {
-    stockList: [],
-  }
 
   const [state, dispatch] = useReducer(StockReducer, initialState)
 
+  const webSocket = useRef<any>()
+
+  const connectToServer = () => {
+    try {
+      if (webSocket.current !== undefined && webSocket.current !== null) {
+        return;
+      }
+
+      webSocket.current = new WebSocket(DEFAULT_URL);
+      webSocket.current.onopen = function () {
+        console.info('Connected to WS')
+        // TODO setConnected()
+      }
+      webSocket.current.onmessage = function (event: any) {
+        console.info('Receiving message')
+        // TODO const data = JSON.parse(event.data)
+      }
+      webSocket.current.onclose = function () {
+        console.info('Disconnected from WS; attempting reconnection in 1 second... ')
+        // TODO setDisconnected()
+
+        setTimeout(() => connectToServer(), 1000)
+      }
+    } catch (event: any) {
+      console.error(event.message)
+    }
+  }
 
   const addStock = (isin: string) => {
     dispatch({
@@ -22,8 +59,8 @@ export const StockProvider: React.FC = ({ children }) => {
   return <StockContext.Provider
     value={{
       stockList: state.stockList,
-
-      addStock
+      connectToServer,
+      addStock,
     }}>
     {children}
   </StockContext.Provider>
